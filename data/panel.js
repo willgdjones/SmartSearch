9ec85b5867e0e153637edc9bb92f33a023a49026
Will Jones <williamgdjones@gmail.com>
2012-08-05 12:53:35 +0100

    Panel which shows visited URls when widget is clicked now works

--- data/panel.js
+++ data/panel.js
@@ -2,6 +2,10 @@ self.port.on('widgetClicked',function createURLList(storedURls){
 	console.log("Working");
 	var URLlist = $('#URLlist');
 	URLlist.empty();
+	storedURls.forEach( function(URL) {
+		URLlist.append(URL + "\n");
+	})
+	//URLlist.html(storedURls);
 
 });
 
