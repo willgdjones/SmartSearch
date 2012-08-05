7974eb888dc8a005f8940ae9ae8cbb7fea460301
Will Jones <williamgdjones@gmail.com>
2012-08-05 14:28:59 +0100

    Added get methods function and drew line on canvas

--- data/panel.js
+++ data/panel.js
@@ -1,3 +1,13 @@
+function getMethods(object) {
+	var methods = [];
+	for (var m in object) {
+		if (typeof object[m] == 'function') {
+			methods.push(m);
+		}
+	}
+	return methods;
+}
+
 self.port.on('widgetClicked',function createURLList(storedURls){
 	console.log("Working");
 	var URLlist = $('#URLlist');
@@ -5,11 +15,21 @@ self.port.on('widgetClicked',function createURLList(storedURls){
 	storedURls.forEach( function(URL) {
 		URLlist.prepend('<p>' + URL + '<p');
 	})
+	//create canvas element in the panel
 	var canvas = $('#canvasDiv canvas');
+	console.log(getMethods(canvas));
+
 	canvas.attr({
 		height: 500,
 		width: 1000
 	});
+	var context = canvas.get(0).getContext('2d');
+	context.beginPath();
+    context.moveTo(100, 150);
+    context.lineTo(450, 50);
+    context.stroke();
+
+
 	//URLlist.html(storedURls);
 
 });
