aaa1acfdc5bf77a5d229f306037ddeea8ab1bfe9
Will Jones <williamgdjones@gmail.com>
2012-08-05 19:58:15 +0100

    In addition to the URLlist, URLs are now stored and linked in a URLobject

--- data/panel.js
+++ data/panel.js
@@ -8,29 +8,33 @@ function getMethods(object) {
 	return methods;
 }
 
-self.port.on('widgetClicked',function createURLList(storedURls){
+self.port.on('widgetClicked',function createURLList(storedURLs){
 	console.log("Working");
 	var URLlist = $('#URLlist');
 	URLlist.empty();
-	storedURls.forEach( function(URL) {
-		URLlist.prepend('<p>' + URL + '<p');
-	})
+	for (var URL in storedURLs) {
+		URLlist.prepend('<p>' + storedURLs[URL] + '</p>');
+	}
+
+	// storedURLs.forEach( function(URL) {
+	// 	URLlist.prepend('<p>' + URL + '</p>');
+	// })
 	//create canvas element in the panel
 	var canvas = $('#canvasDiv canvas');
 	//set the canvas size
 	canvas.attr({
-		height: 500,
-		width: 1000
+		height: 100,
+		width: 200
 	});
 
 	var context = canvas.get(0).getContext('2d');
 	context.beginPath();
-        context.arc(300, 300, 50, 0, 2 * Math.PI, false);
-        context.fillStyle = "#8ED6FF";
-        context.fill();
-        context.lineWidth = 5;
-        context.strokeStyle = "black";
-        context.stroke();
+    context.arc(300, 300, 50, 0, 2 * Math.PI, false);
+    context.fillStyle = "#8ED6FF";
+    context.fill();
+    context.lineWidth = 5;
+    context.strokeStyle = "black";
+    context.stroke();
 
 	//URLlist.html(storedURls);
 
