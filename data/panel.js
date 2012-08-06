113d84c6e583dd444da09e5841c8d37ff0380ef7
Will Jones <williamgdjones@gmail.com>
2012-08-06 22:30:35 +0100

    Successfully got Raphael canvas to work

--- data/panel.js
+++ data/panel.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 function getMethods(object) {
 	var methods = [];
 	for (var m in object) {
@@ -7,6 +12,12 @@ function getMethods(object) {
 	}
 	return methods;
 }
+
+
+
+
+
+
 
 self.port.on('widgetClicked',function createURLList(storedURLs){
 	console.log("Working");
@@ -23,22 +34,43 @@ self.port.on('widgetClicked',function createURLList(storedURLs){
 		}
 	})
 
-	//create canvas element in the panel
-	var canvas = $('#canvasDiv canvas');
-	//set the canvas size
-	canvas.attr({
-		height: 400,
-		width: 200
-	});
+	var paper = new Raphael(document.getElementById('raphaelDiv'),1000,1000);
+	var circ = paper.circle(250, 250, 40);  
+        circ.attr({fill: '#000', stroke: 'none'});
 
-	var context = canvas.get(0).getContext('2d');
-	context.beginPath();
-    context.arc(100, 100, 50, 0, 2 * Math.PI, false);
-    context.fillStyle = "#8ED6FF";
-    context.fill();
-    context.lineWidth = 5;
-    context.strokeStyle = "black";
-    context.stroke();
+    var text = paper.text(250, 250, 'Bye Bye Circle!')  
+	text.attr({opacity: 0, 'font-size': 12}).toBack();  
+
+	circ.node.onmouseover = function() {  
+    this.style.cursor = 'pointer';  
+	}
+
+	circ.node.onclick = function() {  
+    text.animate({opacity: 1}, 2000);  
+    circ.animate({opacity: 0}, 2000, function() {  
+        this.remove();  
+    });  
+	}   
+
+
+
+
+	//create canvas element in the panel
+	// var canvas = $('#canvasDiv canvas');
+	// //set the canvas size
+	// canvas.attr({
+	// 	height: 400,
+	// 	width: 200
+	// });
+
+	// var context = canvas.get(0).getContext('2d');
+	// context.beginPath();
+ //    context.arc(100, 100, 50, 0, 2 * Math.PI, false);
+ //    context.fillStyle = "#8ED6FF";
+ //    context.fill();
+ //    context.lineWidth = 5;
+ //    context.strokeStyle = "black";
+ //    context.stroke();
 
 	//URLlist.html(storedURls);
 
