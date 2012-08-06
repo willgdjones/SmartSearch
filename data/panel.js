2a0f523a06202027a8b5fc8952ad77b9f23ead45
Will Jones <williamgdjones@gmail.com>
2012-08-06 20:56:46 +0100

    Panel now contains two divs, when a URL is clicked in the left div its childURLs appear in the right div

--- data/panel.js
+++ data/panel.js
@@ -13,23 +13,27 @@ self.port.on('widgetClicked',function createURLList(storedURLs){
 	var URLlist = $('#URLlist');
 	URLlist.empty();
 	for (var URL in storedURLs) {
-		URLlist.prepend('<p>' + storedURLs[URL] + '</p>');
+		URLlist.prepend('<p>' + URL + '</p>');
 	}
+	$('#URLlist p').click(function(event) {
+		var $target = $(event.target);
+		$('#childURLs').empty();
+		for (var j in storedURLs[$target.html()]) {
+		$('#childURLs').prepend('<p>' + storedURLs[$target.html()][j] + '</p>');
+		}
+	})
 
-	// storedURLs.forEach( function(URL) {
-	// 	URLlist.prepend('<p>' + URL + '</p>');
-	// })
 	//create canvas element in the panel
 	var canvas = $('#canvasDiv canvas');
 	//set the canvas size
 	canvas.attr({
-		height: 100,
+		height: 400,
 		width: 200
 	});
 
 	var context = canvas.get(0).getContext('2d');
 	context.beginPath();
-    context.arc(300, 300, 50, 0, 2 * Math.PI, false);
+    context.arc(100, 100, 50, 0, 2 * Math.PI, false);
     context.fillStyle = "#8ED6FF";
     context.fill();
     context.lineWidth = 5;
